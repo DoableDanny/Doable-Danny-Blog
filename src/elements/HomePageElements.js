e2d962d09af334bebf4fd38c4829b5ad36e9acd1
Danny <dantheado@gmail.com>
2020-10-22 14:10:16 +0100

    Home page projects section done

--- src/elements/HomePageElements.js
+++ src/elements/HomePageElements.js
@@ -44,25 +44,121 @@ export const ProfileImg = styled(Img)`
 // About me section
 export const AboutSection = styled.section`
     background-color: ${props => props.theme.colors.homeSection};
-    padding: 55px;
+    padding: 30px;
 `
 export const AboutPWrapper = styled.div`
     margin-top: 60px;
     margin-bottom: 60px;
     width: 60%;
+    margin-left: 20px;
+`
+
+// My projects section
+export const ProjectsSection = styled.section`
+    background-color: #000; 
+    padding: 30px;
+`
+
+export const ProjectModulesContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const ProjectModuleWrapper = styled.div`
+    display: flex;
+    ${'' /* background-color: red; */}
+    align-items: center;
+    margin-top: 120px;
+
+    div:first-child {
+        margin-left: 40px;
+    }
+
+    p {
+        ${'' /* font-size: 2.5rem; */}
+        color: "#C5E2E7";
+        line-height: 200% !important;
+        min-width: 290px;
+        margin-top: 10px;
+    }
+
+    @media only screen and (max-width: 55rem) {
+        flex-direction: column;
+        
+        div:first-child {
+        margin-left: 0px;
+        margin-bottom: 20px;
+    }
+    }
+
+
+    @media only screen and (max-width: 400px) {
+        div:first-child {
+            width: 300px;
+        }
+        div:first-child img {
+            width: 300px !important;
+            height: 230px !important;
+        }
+    }
+  
+`
+
+export const ProjectModuleTextWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    ${'' /* background-color: blue; */}
+    width: 600px;
+    margin-left: 100px;
+
+    span:first-child {
+        color: ${props => props.theme.colors.lightBlue};
+        margin-left: 15px;
+    }
+
+    h2 {
+        color: #DCEDF0;
+        margin-bottom: 40px;
+    }
+
+    @media only screen and (max-width: 75rem) {
+        width: 400px;
+    }
+    @media only screen and (max-width: 65rem) {
+        margin-left: 40px;
+        width: 350px;
+    }
+    @media only screen and (max-width: 55rem) {
+        flex-direction: column;
+        margin-left: 0;
+        width: 400px;
+    }
+    @media only screen and (max-width: 400px) {
+        width: 300px;
+    }
+`
+
+export const TechSpan = styled.span`
+    color: #86AFB5 !important;
+    margin-right: 40px;
+    margin-left: 15px;
+
 `
 
 // Home page shared styled components
 export const H = styled.h2`
-    font-size: 2rem;
-    color:#032F35;
+    font-size: 2.3rem;
+    color: ${props => props.color === "light" ? "#E3F1F3" : "#032F35"};
+    padding-left: 20px;
 `
 export const P = styled.p`
     font-size: 2.5rem;
-    color:#032F35;
-    padding-bottom: 20px;
+    color: ${props => props.color === "light" ? "#C5E2E7" : "#032F35"};
+    padding-bottom: 10px;
     line-height: 200% !important;
-    min-width: 290px
+    min-width: 290px;
 `
 
 export const FlexRow = styled.div`
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/Layout"
 import { Seo } from "../components/Seo"
-import {WelcomeSection, WelcomeTextWrapper, ProfileImg, AboutSection, H, P, AboutPWrapper, FlexRow} from '../elements/HomePageElements'
+import {WelcomeSection, WelcomeTextWrapper, ProfileImg, AboutSection, H, P, AboutPWrapper, FlexRow, ProjectsSection,ProjectModulesContainer, ProjectModuleWrapper, ProjectModuleTextWrapper, TechSpan} from '../elements/HomePageElements'
 
 
 export default function Home() {
@@ -15,7 +16,14 @@ export default function Home() {
             ...GatsbyImageSharpFixed
         }
           }
+        },
+     projImg1: file(relativePath: { eq: "proj1.png" }) {
+        childImageSharp {
+          fixed(width: 400, height: 280) {
+            ...GatsbyImageSharpFixed
         }
+          }
+        },
       }
     
     `)
@@ -42,6 +50,49 @@ export default function Home() {
                </FlexRow>
                <H>Skills</H>
            </AboutSection>
+
+           <ProjectsSection>
+              <H color="light">My Projects</H>
+              <ProjectModulesContainer>
+                <ProjectModule image={data.projImg1.childImageSharp.fixed} span="Featured project" title="60 Days of Meditation App" technologies={["React Native", "Firebase"]}>
+                  <P color="light">- A React Native app published to the Google play store with 100s of active users.</P>
+                  <P color="light">- Meditation gamified: complete the current day to unlock the next. Bonus meditations for day streaks and stars!</P>
+                  <P color="light">- Stats such as day streak and average session time saved to local storage.</P>
+                </ProjectModule>
+
+                <ProjectModule image={data.projImg1.childImageSharp.fixed} span="Front-end" title="Calculator" technologies={["ReactJS"]}>
+                  <P color="light">- Built to pass Freecodecamp's front-end-libraries certificate.</P>
+                  <P color="light">- Immediate execution logic e.g. 3 + 5 x 6 - 2 / 4 = 11.5.</P>
+                  <P color="light">- Watermelon theme!</P>
+                </ProjectModule>
+
+                <ProjectModule image={data.projImg1.childImageSharp.fixed} span="Full-stack" title="Exercise Tracker" technologies={["Node, Express, MongoDB"]}>
+                  <p>- Built to pass Freecodecamp's APIs and microservices certificate.</p>
+                  <p>- Users and exercises stored to and accessed from database using mongoose.</p>
+                  <P color="light">- Express handles post and get requests.</P>
+                </ProjectModule>
+             </ProjectModulesContainer>
+           </ProjectsSection>
         </Layout>
     )
+}
+
+function ProjectModule ({image, span, title, children, technologies}) {
+  return(
+    <ProjectModuleWrapper>
+      <div>
+        <Img fixed={image} />
+      </div>
+      <ProjectModuleTextWrapper>
+        <span>{span}</span>
+        <h2>{title}</h2>
+        {children}
+        <FlexRow>
+          {technologies.map((tech) => (
+            <TechSpan>{tech}</TechSpan>
+          ))}
+        </FlexRow>
+      </ProjectModuleTextWrapper>
+    </ProjectModuleWrapper>
+  )
 }
