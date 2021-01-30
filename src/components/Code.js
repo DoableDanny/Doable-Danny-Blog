e541703a16adaf48ee49505899e7efca42afdb86
Dan <dantheado@gmail.com>
2021-01-30 13:36:06 +0000

    Code line highlighting added

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,9 +1,46 @@
+// This Code component is for styling codeBlocks
 import React from "react"
+import rangeParser from "parse-numeric-range"
 import Highlight, { defaultProps } from "prism-react-renderer"
 import theme from "prism-react-renderer/themes/vsDark"
 
-const Code = ({ codeString, language, ...props }) => {
+// Determines if a line of code needs to be highlighted
+const calculateLinesToHighlight = meta => {
+  const RE = /{([\d,-]+)}/
+  if (RE.test(meta)) {
+    const strlineNumbers = RE.exec(meta)[1]
+    const lineNumbers = rangeParser(strlineNumbers)
+    return index => lineNumbers.includes(index + 1)
+  } else {
+    return () => false
+  }
+}
+
+// CodeString is the code to be highlighted, language e.g. "html", metaString = lines to highlight e.g. {1-2, 6}
+const Code = ({ codeString, language, metastring, ...props }) => {
+  const shouldHighlightLine = calculateLinesToHighlight(metastring)
+
   return (
+    // <Highlight
+    //   {...defaultProps}
+    //   code={codeString}
+    //   language={language}
+    //   theme={theme}
+    // >
+    //   {({ className, style, tokens, getLineProps, getTokenProps }) => (
+    //     <div className="gatsby-highlight" data-language={language}>
+    //       <pre className={className} style={style}>
+    //         {tokens.map((line, i) => (
+    //           <div {...getLineProps({ line, key: i })}>
+    //             {line.map((token, key) => (
+    //               <span {...getTokenProps({ token, key })} />
+    //             ))}
+    //           </div>
+    //         ))}
+    //       </pre>
+    //     </div>
+    //   )}
+    // </Highlight>
     <Highlight
       {...defaultProps}
       code={codeString}
@@ -13,13 +50,20 @@ const Code = ({ codeString, language, ...props }) => {
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <div className="gatsby-highlight" data-language={language}>
           <pre className={className} style={style}>
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
+            {tokens.map((line, index) => {
+              const lineProps = getLineProps({ line, key: index })
+              console.log(shouldHighlightLine(index))
+              if (shouldHighlightLine(index)) {
+                lineProps.className = `${lineProps.className} highlight-line`
+              }
+              return (
+                <div key={index} {...lineProps}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              )
+            })}
           </pre>
         </div>
       )}
